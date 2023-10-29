054726453aeef383d8fb914e90b6325710c7f7d3
thaafei <fionaxiework@gmail.com>
2023-10-28 23:16:05 -0400

    fixed not detecting MacBook in string

--- script.py
+++ script.py
@@ -15,11 +15,11 @@ for item in item_links:
     name = item.find("a")
     link = name['href']
     current_price = item.find("div", "as-price-currentprice as-producttile-currentprice")
-    # print("------------")
-    # print(name.text.encode("utf-8"))
-    # print(link)
-    # print(current_price.string.encode("utf-8"))
-    # print("------------")
-    new_row = {'name': name.text.encode("utf-8"), 'price':current_price.string.encode("utf-8"), 'link': link}
-    df= df._append(new_row, ignore_index = True)
-df
+    
+    #cleaning up 
+    name = str(name.text.encode("utf-8"))
+    if "MacBook" in name:
+        name = name.split("b'")[1]
+        new_row = {'name': name, 'price':str(current_price.string.encode("utf-8")), 'link': link}
+        df= df._append(new_row, ignore_index = True)
+print(df)
